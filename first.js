d88a9b2581e134577c11ed46fc4b3f7dd37d52b5
mdrafi276 <mdrafi37473@gmail.com>
2023-08-13 17:42:19 -0700

    first commit

---
+++ first.js
@@ -0,0 +1,19 @@
+var mobile ={
+    brand :"redmi",
+    price:'18000',
+    Storage:'64GB',
+    Ram:'4GB',
+    camra:'48mp'
+
+}
+console.log(mobile)
+var myComputer ={
+    brand:'DELL',
+    price:40000,
+    color:'black',
+    procesor:'intel core er old vrsion',
+    ram:'4GB',
+    stotage:'500GB',
+
+}
+console.log(myComputer.procesor)
